01933f248c00b66a455b89289ed8bf034ae05897
silentrald <silentrald1216@gmail.com>
2024-10-18 13:21:10 +0800

    refactor: remove optional.tpp

--- src/ds/optional.hpp
+++ src/ds/optional.hpp
@@ -8,6 +8,7 @@
 #ifndef DS_OPTIONAL_HPP
 #define DS_OPTIONAL_HPP
 
+#include <type_traits>
 #include <utility>
 
 namespace ds {
@@ -16,48 +17,101 @@ class null_t {};
 const null_t null{};
 
 template <typename T> class optional {
-private:
-  T val{};
-  bool set = false;
-
-  // === Destructor === //
-  void destroy() noexcept;
-
 public:
   optional() noexcept = default;
   optional(const optional&) noexcept = delete;
   optional& operator=(optional&) noexcept = delete;
 
   // === Move === //
-  optional(optional&& rhs) noexcept;
-  optional& operator=(optional&& rhs) noexcept;
-  optional(T&& rhs) noexcept; // NOLINT
-  optional& operator=(T&& rhs) noexcept;
+
+  optional(optional&& other) noexcept : set(other.set) {
+    if (!this->set) {
+      return;
+    }
+
+    this->val = std::move(other.val);
+  }
+
+  optional& operator=(optional&& rhs) noexcept {
+    if (this == &rhs) {
+      return *this;
+    }
+
+    if (!rhs.set) {
+      this->destroy();
+      return *this;
+    }
+
+    this->set = true;
+    this->val = std::move(rhs.val);
+  }
+
+  explicit optional(T&& other) noexcept : val(std::move(other)), set(true) {}
+
+  optional& operator=(T&& rhs) noexcept {
+    this->val = std::move(rhs);
+    this->set = true;
+
+    return *this;
+  }
 
   // === Destructor === //
-  ~optional() noexcept;
+
+  ~optional() noexcept {
+    this->destroy();
+  }
+
+  void destroy() noexcept {
+    if (this->set) {
+      this->set = false;
+      if constexpr (std::is_class<T>::value) {
+        this->val.~T();
+      }
+    }
+  }
 
   // === Null === //
-  optional(const null_t& rhs) noexcept; // NOLINT
-  optional(null_t&& rhs) noexcept;      // NOLINT
-  optional& operator=(const null_t& rhs) noexcept;
-  optional& operator=(null_t&& rhs) noexcept;
+
+  explicit optional(const null_t& rhs) noexcept {}
+  explicit optional(null_t&& rhs) noexcept {};
+
+  optional& operator=(const null_t& rhs) noexcept {
+    this->destroy();
+    return *this;
+  }
+
+  optional& operator=(null_t&& rhs) noexcept {
+    this->destroy();
+    return *this;
+  }
 
   // === Observers === //
 
-  [[nodiscard]] T* operator->() noexcept;
-  [[nodiscard]] const T* operator->() const noexcept;
-  [[nodiscard]] T& operator*() & noexcept;
-  [[nodiscard]] const T& operator*() const& noexcept;
+  [[nodiscard]] T* operator->() noexcept {
+    return &this->val;
+  }
 
-  [[nodiscard]] constexpr explicit operator bool() const noexcept;
+  [[nodiscard]] const T* operator->() const noexcept {
+    return &this->val;
+  }
+
+  [[nodiscard]] T& operator*() & noexcept {
+    return this->val;
+  }
+
+  [[nodiscard]] const T& operator*() const& noexcept {
+    return this->val;
+  }
+
+  [[nodiscard]] constexpr explicit operator bool() const noexcept {
+    return this->set;
+  }
+
+private:
+  T val{};
+  bool set = false;
 };
 
 } // namespace ds
 
-#ifndef DS_OPTIONAL_TPP
-#include "./optional.tpp"
 #endif
-
-#endif
-
